883a64c7331df3f5f03d416e1c432779430a17d9
aapetsi <apetsiampiah@gmail.com>
2019-10-19 04:31:20 +0000

    Add login and logout routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
   <div>
-    <NavLink to='/'>
+    <NavLink to="/">
       <h1>Movie App</h1>
     </NavLink>
     <ul>
       <li>
-        <NavLink to='/'>Home</NavLink>
+        <NavLink to="/">Home</NavLink>
       </li>
       <li>
-        <NavLink to='/browse'>Browse Movies</NavLink>
+        <NavLink to="/browse">Browse Movies</NavLink>
       </li>
       <li>
-        <NavLink to='/saved'>Saved Movies</NavLink>
+        <NavLink to="/saved">Saved Movies</NavLink>
       </li>
       <li>
-        <NavLink to='/watched'>Already Watched</NavLink>
+        <NavLink to="/watched">Already Watched</NavLink>
+      </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
+      <li>
+        <NavLink to="/logout">Logout</NavLink>
       </li>
     </ul>
   </div>
-)
+);
 
-export default Navbar
+export default Navbar;
