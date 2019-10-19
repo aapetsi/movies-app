a93401a683c6e7cf08bc215df3c577bf3232dca8
aapetsi <apetsiampiah@gmail.com>
2019-10-19 13:44:58 +0000

    Complete styling for navbar

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Homepage from '../components/Homepage'
-import Navbar from '../components/Navbar'
-import BrowseMovies from '../components/BrowseMovies'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Homepage from '../components/Homepage';
+import Header from '../components/Header';
+import BrowseMovies from '../components/BrowseMovies';
 
 const AppRouter = () => (
   <BrowserRouter>
-    <Navbar />
+    <Header />
     <Switch>
-      <Route path='/' exact component={Homepage} />
-      <Route path='/browse' component={BrowseMovies} />
+      <Route path="/" exact component={Homepage} />
+      <Route path="/browse" component={BrowseMovies} />
     </Switch>
   </BrowserRouter>
-)
+);
 
-export default AppRouter
+export default AppRouter;
